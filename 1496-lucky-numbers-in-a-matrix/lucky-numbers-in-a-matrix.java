11475099d0e61e3b0c855ed5c106bea9414e0023
Debadrita Dey <124622584+DebadritaDey@users.noreply.github.com>
2025-09-11 11:08:36 +0530

    Time: 5 ms (9.60%) | Memory: 45.6 MB (15.35%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -2,24 +2,35 @@ class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        int maxOfRowMins=Integer.MIN_VALUE;
-        for(int i=0;i<rows;i++){
-            int minVal = matrix[i][0];
-            for(int j=0;j<cols;j++){
-                minVal = Math.min(minVal,matrix[i][j]);
+        HashSet<Integer> minRows = new HashSet<>();
+        HashSet<Integer> maxcols = new HashSet<>();
+        for(int i=0; i<rows; i++){
+            int minval = matrix[i][0];
+            for(int j=0; j<cols; j++){
+                minval = Math.min(matrix[i][j], minval);
             }
-            maxOfRowMins = Math.max(minVal,maxOfRowMins);
+            //store the min rows in the set
+            minRows.add(minval);
         }
+
+        for(int i=0; i<cols; i++){
+            int maxval = matrix[0][i];
+            for(int j=0; j<rows; j++){
+                maxval = Math.max(matrix[j][i], maxval);
+            }
+            //store the max rows in the set
+            maxcols.add(maxval);
+        }
+
         List<Integer> res = new ArrayList<>();
-        for(int i=0;i<cols;i++){ //cols
-            int maxVal = matrix[0][i];
-            for(int j=0;j<rows;j++){ //rows
-                maxVal = Math.max(maxVal,matrix[j][i]);
-            }
-            if(maxVal == maxOfRowMins){
-                res.add(maxVal);
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(minRows.contains(matrix[i][j]) && maxcols.contains(matrix[i][j])){
+                    res.add(matrix[i][j]);
+                }
             }
         }
+
         return res;
     }
 }
